4ffa1d8dc9a267ac99c3b06a8522ac86d0464810
mohamedmodar <mohameddowidar555@gmail.com>
2025-05-04 14:25:56 +0300

    feat: Setup Express.js server with essential middleware
    This commit establishes the foundation for our web
     application by integrating Express.js.
      The following middleware is configured:
    
    -   CORS for cross-origin requests.
    -   Compression for optimized data transfer.
    -   Cookie parsing for handling client-side data.
    -   JSON body parsing for API requests.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,22 @@
-console.log("hello this is the first type script");
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+  credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(9000, ()=>{
+    console.log("server running on http://localhost:9000/")
+})
